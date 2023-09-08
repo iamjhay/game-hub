a2dad9cc1c31a6f026a6ba3cfb2217dd150efe4f
John Edet <jhaydrille75@gmail.com>
2023-09-08 09:58:05 +0100

    Displaying Critic Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 // import getCroppedImageUrl from "../services/image-url";
-// import CriticScore from "./CriticScore";
+import CriticScore from "./CriticScore";
 // import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
 
@@ -18,7 +18,7 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList
             platforms={game.parent_platforms?.map((p) => p.platform)}
           />
-          {/* <CriticScore score={game.metacritic} /> */}
+          <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
           {game.name}
