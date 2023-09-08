d592da0f857e8650d4937f454f7f69f4742016a2
John Edet <jhaydrille75@gmail.com>
2023-09-08 09:50:43 +0100

    Displaying Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import { Game } from "../hooks/useGames";
 // import getCroppedImageUrl from "../services/image-url";
 // import CriticScore from "./CriticScore";
 // import Emoji from "./Emoji";
-// import PlatformIconList from "./PlatformIconList";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -14,12 +14,12 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={game.background_image} />
       <CardBody>
-        {/* <HStack justifyContent="space-between" marginBottom={3}>
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms?.map((p) => p.platform)}
           />
-          <CriticScore score={game.metacritic} />
-        </HStack> */}
+          {/* <CriticScore score={game.metacritic} /> */}
+        </HStack>
         <Heading fontSize="2xl">
           {game.name}
           {/* <Emoji rating={game.rating_top} /> */}
